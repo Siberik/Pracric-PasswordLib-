9aefc9dd15736571412d1fcf41b24b9b9916638e
Vladislav <87132317+Siberik@users.noreply.github.com>
2022-10-05 17:57:04 +0500

    first commit

---
+++ PasswordClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordLibarary2
+{
+    public class PasswordClass
+    {
+        /// <summary>
+        /// Проверка на сложность пароля
+        /// </summary>
+        /// <param name="password">
+        /// Является паролем
+        /// </param>
+        /// <returns>
+        /// Возвращает сложность пароля
+        /// </returns>
+        public static int PasswordStrengthCheker(string password)
+        {
+            var count = 0;
+
+            //метод, проверящий сложность пароля от 7 символов и более
+            if (password == @".{7,}")
+            {
+                count = count + 1;
+
+                //метод, проверяющий наличие в пароле нижнего регистра
+                if (password == @"(?=.*[a-z])")
+                {
+                    count = count + 1;
+                }
+                //метод, проверящий наличие в пароле верхнего регистра 
+                if (password == @"(?=.*[A-Z])")
+                {
+                    count = count + 1;
+                }
+                //метод, проверяющий наличие в пароле цифр
+                if (password == @"(?=.*\d)")
+                {
+                    count = count + 1;
+                }
+                //метод, проверяющий наличие Кириллицы в пароле
+                if (password == @"(?=.*[А-Я])||(?=.*[а-я])")
+                {
+                    throw new Exception("Кириллические символы запрещены при вводе пароля");
+                }
+                //метод, проверяющий наличие спец символов
+                if (password == @"[\!\@\\\*\(\)]")
+                {
+                    count = count + 1;
+                }
+            }
+
+
+
+
+
+            return count;
+        }
+        /// <summary>
+        /// Проверка правильности формирования пароля
+        /// </summary>
+        /// <param name="password">
+        /// Пароль
+        /// </param>
+        /// <returns>
+        /// Выводит, истина или ложь(подходит ли нам пароль или нет)
+        /// </returns>
+        public bool CheckPassword(string password)
+        {
+            if (password == @"((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15})")
+            {
+                if (password == @"[\!\@\\\*\(\)\^\$\]")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
---
+++ PasswordLibraryTests/PasswordClassTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using PasswordLibarary2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PasswordLibraryTests
+{
+    [TestClass]
+    public class PasswordClassTests
+    {
+        /// <summary>
+        /// Проверка с помощью функции подходит ли пароль нашим условиям
+        /// </summary>
+        [TestMethod]
+        public void CheckPassword_StringEmpty_ReturnedFalse()
+        {
+            //Arrange
+            string password = string.Empty;
+
+            //Act
+            PasswordClass c = new PasswordClass();
+            bool actual = c.CheckPassword(password);
+
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка слишком короткого пароля
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_ShortPassword_Returned0()
+        {
+            //Arrange
+            string password = "123a";
+            int excepted = 0;
+            //Act
+
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+
+            //Assert
+            Assert.AreEqual(actual, excepted);
+        }
+        /// <summary>
+        /// Проверка пустую строку
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_OnlyNumbers_Returned1()
+        {
+            //Arrange
+            string password = "";
+            int excepted = 0;
+            //Act
+
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+
+            //Assert
+            Assert.AreEqual(actual, excepted);
+        }
+        /// <summary>
+        /// Проверка пустую строку
+        /// </summary>
+        [TestMethod]
+        public void PasswordStrengthCheker_UpperLoewerAndNumber_Returned1()
+        {
+            //Arrange
+            string password = "Passw0rd";
+            int excepted = 0;
+            //Act
+
+            int actual = PasswordClass.PasswordStrengthCheker(password);
+
+            //Assert
+            Assert.AreEqual(actual, excepted);
+        }
+        /// <summary>
+        /// Проверка только цифры
+        /// </summary>
+        [TestMethod]
+        public void CheckPassword_OnlyLetters_ReturnedTrue()
+        {
+            //Arrange
+            string password = "12341236401";
+
+            //Act
+            PasswordClass c = new PasswordClass();
+            bool actual = c.CheckPassword(password);
+
+            //Assert
+            Assert.IsFalse(actual);
+        }
+
+    }
+}
